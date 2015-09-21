4f580cbeda34ac8cd3f1c812b3caae27c3ab1ed1
pobono-admin <pobono.liren@gmail.com>
2015-09-21 21:06:06 +0800

    password reset page and controller

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,5 +7,10 @@
     mail to: user.email, subject: "Account activation"
   end
 
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "Password reset"
+  end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -67,6 +67,11 @@ class User < ActiveRecord::Base
   end
 
 
+   def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+   end 
+
+
  private
 
     # Converts email to all lower-case.
