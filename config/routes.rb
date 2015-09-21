0149e5eb25a1217008252bb72809852ae8fab43a
pobono-admin <pobono.liren@gmail.com>
2015-09-21 18:02:48 +0800

    forget password

--- config/routes.rb
+++ config/routes.rb
@@ -60,6 +60,8 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+
 
   root :to      => "welcome#index"
   get "welcome" => "welcome#index"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150916081411) do
+ActiveRecord::Schema.define(version: 20150921033303) do
 
   create_table "microposts", force: true do |t|
     t.text     "content"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20150916081411) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
