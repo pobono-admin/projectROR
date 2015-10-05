645bf657ea4edd564aa274bb2401fd41f66c2ce3
pobono-admin <pobono.liren@gmail.com>
2015-10-05 17:38:43 +0800

    micropost

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
 
       if user.activated?
         log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        # params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_back_or user
       else
         message  = "Account not activated. "
