f769a983daa43f78a996a6e8d1e59ca21dda73e3
pobono-admin <pobono.liren@gmail.com>
2015-10-12 14:30:31 +0800

    upload photo

--- db/migrate/20151008033530_create_microposts.rb
+++ db/migrate/20151008033530_create_microposts.rb
@@ -2,10 +2,15 @@ class CreateMicroposts < ActiveRecord::Migration
   def change
     create_table :microposts do |t|
       t.text :content
+      t.text :title
+
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
     end
     add_index :microposts, [:user_id, :created_at]
+    add_column :microposts , :note, :text
   end
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151007071042) do
+ActiveRecord::Schema.define(version: 20151012024929) do
 
   create_table "microposts", force: true do |t|
     t.text     "content"
+    t.text     "title"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "note"
+    t.string   "picture"
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
