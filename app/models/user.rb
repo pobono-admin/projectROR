5dec69ca4c291afbb143d4cd384441de518b2ffe
pobono-admin <pobono.liren@gmail.com>
2015-11-19 14:01:56 +0800

    follow seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,9 @@ class User < ActiveRecord::Base
   has_many :passive_relationships, class_name:  "Relationship",
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
-                                   
   has_many :following, through: :active_relationships,  source: :followed
   has_many :followers, through: :passive_relationships, source: :follower                                                                     
+
 
   # A hash password
 	has_secure_password
@@ -95,6 +95,21 @@ class User < ActiveRecord::Base
     # See "Following users" for the full implementation.
     def feed
       microposts
+    end
+
+    # Follows a user.
+    def follow(other_user)
+      active_relationships.create(followed_id: other_user.id)
+    end
+
+    # Unfollows a user.
+    def unfollow(other_user)
+      active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+      following.include?(other_user)
     end
 
 
